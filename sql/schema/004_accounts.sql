637208a157e1c3a70cbac1597f3ebe8577334ecd
jgopie <jordangopie@live.com>
2024-12-04 20:37:05 -0400

    Add accounts table

---
+++ sql/schema/004_accounts.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+CREATE TABLE accounts(
+    id UUID PRIMARY KEY,
+    account_name TEXT NOT NULL,
+    current_balance MONEY NOT NULL,
+    account_type TEXT NOT NULL,
+    user_id UUID NOT NULL,
+    CONSTRAINT fk_user_id FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE accounts;
